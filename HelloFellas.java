b37311ea87f4f168c536983fd39753138ae14046
Vishakha Motwani <vishakha.motwani@gmail.com>
2016-09-11 00:52:17 -0400

    The usual Hello Fellas sample

---
+++ HelloFellas.java
@@ -0,0 +1,12 @@
+/*
+HelloFellas.java
+Testing Visual Studio Code
+*/
+
+public class HelloFellas
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Fellas!");
+    }
+}
